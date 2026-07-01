3835671e30c27a37b40aaab1fd1a86c162602214
Willian Teixeira <ws.teixeira@outlook.com>
2026-07-01 10:28:24 -0300

    feat: adiciona manipulacao e limpeza do formulario

--- js/api.js
+++ js/api.js
@@ -7,7 +7,24 @@ const api = {
             alert("Erro ao buscar dados");
             throw error
         }
+    },
+
+    async salvaPensamento (pensamento) {
+        try {
+            const response = await fetch("http://localhost:3000/pensamentos", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch (error) {
+            alert("Erro ao salvar o pensamento");
+            throw error
+        }
     }
 }
+
 
 export default api;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-    ui.renderizarPensamentos()
+    ui.renderizarPensamentos();
+
+    const formularioPensamento = document.getElementById("pensamento-form");
+    const botaoCancelar = document.getElementById("botao-cancelar");
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario);
+    botaoCancelar.addEventListener("click", manipulaLimpeza);
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault();
+    const id = document.getElementById("pensamento-id").value;
+    const conteudo = document.getElementById("pensamento-conteudo").value;
+    const autoria = document.getElementById("pensamento-autoria").value;
+
+    try {
+        await api.salvaPensamento({ conteudo, autoria });
+        ui.renderizarPensamentos();
+    } catch (error) {
+        alert("Erro ao salvar pensamentos");
+        throw error;
+    }
+}
+
+function manipulaLimpeza() {
+    ui.limpaFormulario()
+}
