048992890684b477f73f00e6ada75fdcd2281d23
Willian Teixeira <ws.teixeira@outlook.com>
2026-07-03 10:21:55 -0300

    feat: cria funcao para buscar/editar um pensamento

--- js/api.js
+++ js/api.js
@@ -1,5 +1,5 @@
 const api = {
-    async buscarPensamentos () {
+    async buscarPensamentos() {
         try {
             const response = await fetch("http://localhost:3000/pensamentos");
             return await response.json();
@@ -9,18 +9,44 @@ const api = {
         }
     },
 
-    async salvaPensamento (pensamento) {
+    async salvaPensamento(pensamento) {
         try {
             const response = await fetch("http://localhost:3000/pensamentos", {
                 method: "POST",
                 headers: {
-                    "content-type": "application/json"
+                    "Content-type": "application/json"
                 },
                 body: JSON.stringify(pensamento)
             });
             return await response.json();
         } catch (error) {
             alert("Erro ao salvar o pensamento");
+            throw error
+        }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`);
+            return await response.json();
+        } catch (error) {
+            alert("Erro ao buscar pensamento");
+            throw error
+        }
+    },
+
+    async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch (error) {
+            alert("Erro ao editar o pensamento");
             throw error
         }
     }
--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,15 @@
 import api from "./api.js";
 
 const ui = {
+
+    async preencherFormulario(pensamentoId) {
+        const pensamento = await api.buscarPensamentoPorId(pensamentoId);
+        document.getElementById("pensamento-id").value = pensamento.id;
+        document.getElementById("pensamento-conteudo").value = pensamento.conteudo;
+        document.getElementById("pensamento-autoria").value = pensamento.autoria;
+        
+    },
+
     async renderizarPensamentos() {
         const listaPensamentos = document.getElementById("lista-pensamentos");
 
@@ -15,29 +24,38 @@ const ui = {
 
     criaElementoPensamento(pensamento) {
         const listaPensamentos = document.getElementById("lista-pensamentos");
-        const li = document.createElement("li")
-        li.setAttribute("data-id", pensamento.id)
-        li.classList.add("li-pensamento")
+        const li = document.createElement("li");
+        li.setAttribute("data-id", pensamento.id);
+        li.classList.add("li-pensamento");
 
-        const iconeAspas = document.createElement("img")
-        iconeAspas.src = "assets/imagens/aspas-azuis.png"
-        iconeAspas.alt = "Aspas azuis"
-        iconeAspas.classList.add("icone-aspas")
+        const iconeAspas = document.createElement("img");
+        iconeAspas.src = "assets/imagens/aspas-azuis.png";
+        iconeAspas.alt = "Aspas azuis";
+        iconeAspas.classList.add("icone-aspas");
 
-        const pensamentoConteudo = document.createElement("div")
-        pensamentoConteudo.textContent = pensamento.conteudo
-        pensamentoConteudo.classList.add("pensamento-conteudo")
+        const pensamentoConteudo = document.createElement("div");
+        pensamentoConteudo.textContent = pensamento.conteudo;
+        pensamentoConteudo.classList.add("pensamento-conteudo");
         
-        const pensamentoAutoria = document.createElement("div")
-        pensamentoAutoria.textContent = pensamento.autoria
-        pensamentoAutoria.classList.add("pensamento-autoria")
+        const pensamentoAutoria = document.createElement("div");
+        pensamentoAutoria.textContent = pensamento.autoria;
+        pensamentoAutoria.classList.add("pensamento-autoria");
 
+        const botaoEditar = document.createElement("button");
+        botaoEditar.classList.add("botao-editar");
+        botaoEditar.onclick = () => ui.preencherFormulario(pensamento.id);
 
-        li.appendChild(iconeAspas)
-        li.appendChild(pensamentoConteudo)
-        li.appendChild(pensamentoAutoria)
+        const iconeEditar = document.createElement("img");
+        iconeEditar.src = "assets/imagens/icone-editar.png";
+        iconeAspas.alt = "Editar";
+        botaoEditar.append(iconeEditar);
 
-        listaPensamentos.appendChild(li)
+        const icones = document.createElement("div");
+        icones.classList.add("icones");
+        icones.append(botaoEditar);
+
+        li.append(iconeAspas, pensamentoConteudo, pensamentoAutoria, icones);
+        listaPensamentos.append(li);
     },
 
     limpaFormulario() {
